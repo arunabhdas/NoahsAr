0475498ad52866e1769eebc2a685350fc7e9ec53
Arunabh Das <arunabhdas@gmail.com>
2018-05-30 12:55:03 -0400

    Added segue to FirstViewController

---
+++ NoahsAr/NoahsAr/InitialViewController.swift
@@ -0,0 +1,38 @@
+//
+//  InitialViewController.swift
+//  NoahsAr
+//
+//  Created by Das on 5/30/18.
+//  Copyright © 2018 arunabhdas. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+	@IBAction func buttonTapped(_ sender: Any) {
+		self.performSegue(withIdentifier: "InitialToFirstViewController", sender: nil)
+	}
+	override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
