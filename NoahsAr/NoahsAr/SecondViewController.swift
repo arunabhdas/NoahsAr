5a72bc9e0ff898732d9425fd61e54e5bb1330d52
Arunabh Das <arunabhdas@gmail.com>
2018-05-31 03:57:40 -0400

    Added basketball scene assets

--- NoahsAr/NoahsAr/SecondViewController.swift
+++ NoahsAr/NoahsAr/SecondViewController.swift
@@ -17,12 +17,54 @@ class SecondViewController: UIViewController, ARSCNViewDelegate {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+		
+		// Set the view's delegate
+		self.sceneView.delegate = self
+		
+		// Show statistics such as fps and timing information
+		self.sceneView.showsStatistics = true
+		
+		// Create a new scene
+		// let scene = SCNScene(named: "art.scnassets/hoop.scn")!
+		
+		// self.sceneView.scene = scene
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+	func addBackboard() {
+		guard let backboardScene = SCNScene(named: "art.scnassets/hoop.scn") else {
+			return
+		}
+		
+		guard let backboardNode = backboardScene.rootNode.childNode(withName: "backboard", recursively: false) else {
+			return
+		}
+		
+		backboardNode.position = SCNVector3(x: 0, y: 0.0, z: 0.0)
+		
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		// Create a session configuration
+		let configuration = ARWorldTrackingConfiguration()
+		
+		// Run the view's session
+		sceneView.session.run(configuration)
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		
+		// Pause the view's session
+		sceneView.session.pause()
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Release any cached data, images, etc that aren't in use.
+	}
     
 	// MARK: - ARSCNViewDelegate
 	
